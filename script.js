61bea10b8d87db13d4333299f05250c756da7256
ofcoursevruv <learny023@gmail.com>
2025-01-17 23:53:06 +0530

    First commit: functions for computer's choice and user's choice done

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  const compChoicesForTheGame = ["Rock", "Paper", "Scissor"];
+  let indexOfChoice = Math.floor(Math.random() * 3);
+  return compChoicesForTheGame[indexOfChoice];
+}
+
+function getHumanChoice() {
+  let humanInput = window.prompt(
+    `Enter either "Rock", "Paper", or "Scissor": `
+  );
+  humanInput = humanInput[0].toUpperCase() + humanInput.slice(1).toLowerCase();
+  return humanInput;
+}
+
+console.log(getHumanChoice());
